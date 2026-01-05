bd4de7f6257b2e100f2ea5e632b254df0d431e39
Sushmitha K <sushmithagowda712@.gmail.com>
2026-01-05 18:18:57 +0530

    Add SendGrid mail dependency

--- cron_runner.cjs
+++ cron_runner.cjs
@@ -33,9 +33,9 @@ async function runJob(label) {
  */
 jobStatus.reset();
 
-cron.schedule("59 17 * * *", () => runJob("59:17"));
-cron.schedule("05 18 * * *", () => runJob("05:18"));
-cron.schedule("10 18 * * *", () => runJob("10:18"));
+cron.schedule("20 18 * * *", () => runJob("20:18"));
+cron.schedule("25 18 * * *", () => runJob("25:18"));
+cron.schedule("30 18 * * *", () => runJob("30:18"));
 
 cron.schedule("0 0 * * *", () => {
   console.log("🌙 Midnight reset");
